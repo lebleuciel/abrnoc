8e9a3e1ce6e9b48041f0d0a1e5d424f8eeb11ba4
lebleuciel <Hastidarabi612@gmail.com>
2023-02-18 15:14:58 +0330

    add controllers

---
+++ controller/getControllers.js
@@ -0,0 +1,24 @@
+exports.landing = (req, res) => {
+    res.send('working!!');
+    console.log('hello from landing!!');
+};
+
+exports.showSubscriptions = (req, res) => {
+    res.send('working!!');
+    console.log('hello from showSubscriptions!!');
+};
+
+exports.showInvoices = (req, res) => {
+    res.send('working!!');
+    console.log('hello from showInvoices!!');
+};
+
+exports.showAllInvoices = (req, res) => {
+    res.send('working!!');
+    console.log('hello from showAllInvoices!!');
+};
+
+exports.showAllExpences = (req, res) => {
+    res.send('working!!');
+    console.log('hello from showAllExpences!!');
+};
---
+++ controller/postControllers.js
@@ -0,0 +1,78 @@
+var user = require('../model/userModel');
+var subscriptionModel = require('../model/subscriptionModel');
+
+exports.toggleSubscription = (req, res) => {
+    console.log('hello from toggleSubscription!!');
+
+    subscriptionModel
+        .findByIdAndUpdate(req.body.id, {
+            status: req.body.status,
+        })
+        .then((subscription) => {
+            if (!subscription)
+                return res.status(404).json({
+                    status: 'error',
+                    message: `error: subscription not found`,
+                });
+
+            console.log('subscription updated');
+            console.log(subscription);
+
+            res.status(200).json({
+                status: 'success',
+                message: `subscription ${subscription.name} updated successfully`,
+                id: subscription._id,
+            });
+        })
+        .catch((error) => {
+            res.status(500).json({
+                status: 'error',
+                message: `error: ${error.message}`,
+            });
+        });
+};
+
+exports.newSubscription = (req, res) => {
+    console.log('hello from newSubscription!!');
+
+    subscriptionModel
+        .create(req.body)
+        .then((newSubscription) => {
+            console.log('new subscription saved');
+            console.log(newSubscription);
+
+            res.status(200).json({
+                status: 'success',
+                message: `subscription ${newSubscription.name} saved successfully`,
+                id: newSubscription._id,
+            });
+        })
+        .catch((error) => {
+            res.status(500).json({
+                status: 'error',
+                message: `error: ${error.message}`,
+            });
+        });
+};
+
+exports.createUser = (req, res) => {
+    console.log('hello from createUser!!');
+
+    user.create(req.body)
+        .then((newUser) => {
+            console.log('new person saved');
+            console.log(newUser);
+
+            res.status(200).json({
+                status: 'success',
+                message: `user ${newUser.username} saved successfully`,
+                id: newUser._id,
+            });
+        })
+        .catch((error) => {
+            res.status(500).json({
+                status: 'error',
+                message: `error: ${error.message}}`,
+            });
+        });
+};
