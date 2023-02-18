bf527ff5539aec7648c7ac0f7933355f4896bd38
lebleuciel <Hastidarabi612@gmail.com>
2023-02-18 15:31:18 +0330

    add one endpoint in get controller

--- controller/getControllers.js
+++ controller/getControllers.js
@@ -1,11 +1,41 @@
+var subscriptionModel = require('../model/subscriptionModel');
+
 exports.landing = (req, res) => {
     res.send('working!!');
     console.log('hello from landing!!');
 };
 
 exports.showSubscriptions = (req, res) => {
-    res.send('working!!');
     console.log('hello from showSubscriptions!!');
+
+    subscriptionModel
+        .find({
+            user_id: req.body.user_id,
+        })
+        .then((data) => {
+            console.log(data);
+            let result = [];
+            data.forEach((row) => {
+                result.push({
+                    id: row._id,
+                    price: row.price,
+                    status: row.status,
+                    created_at: row.created_at,
+                });
+            });
+
+            res.status(200).json({
+                status: 'success',
+                message: `get user: ${req.body.user_id} subscriptions successfully`,
+                data: result,
+            });
+        })
+        .catch((error) => {
+            res.status(500).json({
+                status: 'error',
+                message: `error: ${error.message}`,
+            });
+        });
 };
 
 exports.showInvoices = (req, res) => {
