2865289c66064b18d54a4ab716701966f2d22073
lebleuciel <Hastidarabi612@gmail.com>
2023-02-18 15:06:43 +0330

    add env and update server

--- server.js
+++ server.js
@@ -1,4 +1,35 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+let dotenvResult;
+
+if (process.argv.indexOf('--config') !== -1)
+    dotenvResult = dotenv.config({
+        path: path.join(
+            __dirname,
+            '/',
+            process.argv[process.argv.indexOf('--config') + 1]
+        ),
+    });
+else
+    dotenvResult = dotenv.config({
+        path: path.join(__dirname, '.env/dev.env'),
+    });
+
+if (dotenvResult.error) {
+    console.error(dotenvResult.error);
+    process.exit();
+}
+
 const app = require('./app');
+const mongoose = require('mongoose');
+//to fix strict query error
+mongoose.set('strictQuery', false);
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
 
 const startApp = (port) => {
     const server = app.routes
@@ -8,6 +39,16 @@ const startApp = (port) => {
         .on('error', (err) => {
             reject(err);
         });
-}
+};
 
-startApp("3000");
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+    })
+    .then(() => {
+        console.log('connection succesful');
+        startApp(process.env.PORT);
+    })
+    .catch((error) => {
+        console.error('Error connecting to the database:', error);
+    });
